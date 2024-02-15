aa10a2b336e9572bf333de357bf24e2adbb541b1
Bitals <me@bitals.xyz>
2024-02-15 19:37:28 +0400

    Add some comments

--- init.lua
+++ init.lua
@@ -8,11 +8,14 @@ return {
     group = 'AutoRestore',
     once = true,
     callback = function(_)
+      -- Load last closed session if launched from ~ with no arguments
       local stargs = vim.fn.argc()
       local vcwd = vim.fn.getcwd()
       local homedir = os.getenv( "HOME" )
       if ( stargs == 0 and vcwd == homedir ) then
+        -- Skip if another instance is already running
         local nvims = tonumber(io.popen("pgrep -x nvim|wc -l"):read("*a"))
+        -- Every instance spawns 2 processes IDK why
         if nvims>2 then
             -- require("alpha").start(false)
             -- vim.cmd.Neotree('toggle')
@@ -20,11 +23,12 @@ return {
         else
           vim.cmd.SessionManager('load_last_session')
         end
+      -- Load expliticly specified non-~ directory session if it exists
       elseif ( stargs == 0 and vcwd ~= homedir ) then
-        local vsession = require('session_manager.config').dir_to_session_filename(vcwd)
-        if vsession:exists() then
+        if require('session_manager.config').dir_to_session_filename(vcwd):exists() then
           vim.cmd.SessionManager('load_current_dir_session')
         else
+          -- Just open neo-tree if it doesn't
           vim.cmd.Neotree('toggle')
           -- vim.cmd.wincmd "p"
         end
@@ -32,6 +36,7 @@ return {
    end,
   }),
 
+  -- Open neo-tree upon session load because I'm a child
   vim.api.nvim_create_autocmd({ 'User' }, {
     pattern = "SessionLoadPost",
     group = 'AutoRestore',
